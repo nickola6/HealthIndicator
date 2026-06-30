cc5af9b2e58a16f8ad9388596d02416e85c08c1b
nick <kolya.2003.yskov@mail.ru>
2026-06-30 20:48:16 +0300

    Adding events to the code and removing them from the Unity Inspector

--- Scripts/Attacker.cs
+++ Scripts/Attacker.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Attacker : MonoBehaviour
 {
+    [SerializeField] private Button _damageButton;
     [SerializeField] private Health _health;
     [SerializeField] private float _damage = 15f;
 
-    public void Attack()
+    private void OnEnable()
+    {
+        _damageButton.onClick.AddListener(Attack);
+    }
+
+    private void OnDisable()
+    {
+        _damageButton.onClick.RemoveListener(Attack);
+    }
+
+    private void Attack()
     {
         _health.TakeDamage(_damage);
     }
--- Scripts/Healer.cs
+++ Scripts/Healer.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healer : MonoBehaviour
 {
+    [SerializeField] private Button _healButton;
     [SerializeField] private Health _health;
     [SerializeField] private float _healValue = 10f;
 
-    public void Heal()
+    private void OnEnable()
+    {
+        _healButton.onClick.AddListener(Heal);
+    }
+
+    private void OnDisable()
+    {
+        _healButton.onClick.RemoveListener(Heal);
+    }
+
+    private void Heal()
     {
         _health.Heal(_healValue);
     }
